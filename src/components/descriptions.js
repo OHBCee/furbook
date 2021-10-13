29c21c83880b5d7f5cef7129a5b0ac7653d47ef8
OHBCee <90053519+OHBCee@users.noreply.github.com>
2021-10-13 12:26:31 -0400

    Todays Work Word

--- src/components/descriptions.js
+++ src/components/descriptions.js
@@ -6,31 +6,16 @@ function Descriptions() {
     <div className="container-fluid Descriptions">
       <div className="row">
         <div className="col">
-          <h5 className="title-text">Real Cats</h5>
-          <p className="sub-text">
-            Contrary to popular belief, cats aren't loners. At furbook, we know
-            cats need friendship, affection, and comradery. It is our core
-            belief that beneath every cat's hard, angry, exterior there is a pet
-            who jus wants to play and cuddle with a friend
-          </p>
+          <h5 className="title-text">Rare Cats</h5>
+          <p className="sub-text">This is a Cat!</p>
         </div>
         <div className="col">
-          <h5 className="title-text">Real Dogs</h5>
-          <p className="sub-text">
-            Dogs are man's best friend. But dogs can also be dog's best friend.
-            Maybe even bester than man's best friend. On furbook, you can find
-            millions of other dogs who can be a loyal companion, trusted
-            confidant, and reliable wrestling partner
-          </p>
+          <h5 className="title-text">Regular Dogs</h5>
+          <p className="sub-text">This is a Dog!</p>
         </div>
         <div className="col">
-          <h5 className="title-text">Real Friends</h5>
-          <p className="sub-text">
-            s Who says cats and dogs can't get along? Here at Furbook, we
-            believe all pets can be best friends. Our furbook users believe it
-            doesn't matter if you are a cat, dog, rabbit, or ferret; underneath
-            that fur we are all cute, wonderful pets.{" "}
-          </p>
+          <h5 className="title-text">Anual CatsvsDogs Race!</h5>
+          <p className="sub-text">This is a dog running!</p>
         </div>
       </div>
     </div>
--- src/components/splash.js
+++ src/components/splash.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import './css/splash.css';
+import React from "react";
+import "./css/splash.css";
 
 function Splash() {
-  return(
-    <div className='Splash'>
-      <h1 className='primary text'>Furbook</h1>
-      <h5 className='secondary text'>Where pets aren't just man's best friend</h5>
+  return (
+    <div className="Splash">
+      <h1 className="primary text">Pets n' Stuff</h1>
+      <h5 className="secondary text">
+        Shop here or you will catch COVID (you will survive)!
+      </h5>
+      <h5 className="secondarytext">Hi</h5>
     </div>
   );
 }
